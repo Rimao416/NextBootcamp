f7092911d5c1ed5dc5d1e43316b78ef3bb14d913
Rimao416 <kayumbaomari27@gmail.com>
2025-02-23 01:40:48 +0100

    09_LoadingUi

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Template
+# LoadingUi
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,22 @@
+import { Metadata } from "next";
 import React from "react";
 
-function page() {
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
+
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Me");
+    }, 2000); // Délai de 2000 millisecondes (2 secondes)
+  });
+
   return (
     <div>
       <h1>Je suis la page blog</h1>
     </div>
   );
 }
-
-export default page;
