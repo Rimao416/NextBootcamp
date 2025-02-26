657c14757d020698286e6d3861c07f8a6af277b8
Rimao416 <kayumbaomari27@gmail.com>
2025-02-26 11:51:29 +0100

    15_CookiesRoutes

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,8 +1,19 @@
-import { headers } from "next/headers";
+import { headers,cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const headerList = await headers();
+  const theme=request.cookies.get("theme")
+  console.log(theme)
+
+  const cookieStore=await cookies()
+  cookieStore.set("resultsPerPage","20")
+  console.log(cookieStore.get("resultsPerPage"))
   console.log(headerList.get("Authorization"));
-  return new Response("Profile API Data");
+  return new Response("Profile API Data", {
+    headers: {
+      "Content-Type": "application/json",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
