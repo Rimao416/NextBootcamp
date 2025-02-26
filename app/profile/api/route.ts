c4de5e37f4c2b4bd3d2007227ff0e024fdf6bd65
Rimao416 <kayumbaomari27@gmail.com>
2025-02-26 11:27:27 +0100

    14_HeadersInRouteHandlers

--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,3 +1,8 @@
-export async function GET() {
-  return new Response("Hello Next.js!");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+  return new Response("Profile API Data");
 }
